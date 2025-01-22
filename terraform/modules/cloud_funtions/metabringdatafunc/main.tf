8fddaccff668c723d989ed120fad6642cf9076e7
jatindersinghmalhi <jack@jbmalhigroup.com>
2025-01-22 15:46:56 +1100

    cloud function created to fetch data weekly from stock market

--- terraform/modules/cloud_funtions/metabringdatafunc/main.tf
+++ terraform/modules/cloud_funtions/metabringdatafunc/main.tf
@@ -65,6 +65,7 @@ resource "google_cloudfunctions2_function" "func_trigger_bucket_to_bigquery" {
     timeout_seconds    = 60
     environment_variables = {
       SERVICE_CONFIG_TEST = "config_test"
+      SERVICE_CONFIG_DIFF_TEST = var.service_account_email
       TABLE_ID = var.table_id
       SYMBOL = var.symbol
       # API_KEY =var.meta_api_token
