bbae94930ce0fba770e0eceb368c0ea7b9e629e6
jatindersinghmalhi <jack@jbmalhigroup.com>
2025-01-24 10:28:51 +1100

    destroying all resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,58 @@
-######
-module "workload_identity" {
-  source = "./modules/workload_identity_federation"
-  project_id=var.project_id
-  pool_id = var.pool_id
-  repo = var.repo
-  org_id = var.org_id
-  project_number = var.project_number
-  service_account_id = var.service_account_id
-}
-######
-module "cloudbuild" {
-  source = "./modules/cloud_build"
-  project_id = var.project_id
-  region = var.region
-  project_number = var.project_number
-  installation_id = var.installation_id
-  cloud_build_github_pat = var.cloud_build_github_pat
-  secret_id = var.secret_id
-  remote_uri = var.remote_uri
-}
-######
-module "artifact_registry" {
-  source = "./modules/artifact_registry"
-  project_id = var.project_id
-  region = var.region
-}
-######
-module "vertex_ai_workbench" {
-  source = "./modules/vetex_ai_workbench"
-  project_id = var.project_id
-  service_account_email = var.service_account_id
-}
-######
-module "big-query" {
-  source = "./modules/big_query"
-  project_id = var.project_id
-  region = var.region
-  
-}
-######
-module "cloud_function" {
-  source = "./modules/cloud_funtions/metabringdatafunc"
-  project_id = var.project_id
-  region = var.region
-  service_account_email = var.service_account_id
-  table_id = "${var.project_id}.${module.big-query.meta_dataset_id}.${module.big-query.meta_table_id}"
-  meta_api_secret_id = var.meta_api_secret_id
-  meta_api_token = var.meta_api_token
-  project_number = var.project_number
-}
-##########
-# module "datafrom_repository" {
-#   source = "./modules/dataform"
+# ######
+# module "workload_identity" {
+#   source = "./modules/workload_identity_federation"
+#   project_id=var.project_id
+#   pool_id = var.pool_id
+#   repo = var.repo
+#   org_id = var.org_id
+#   project_number = var.project_number
+#   service_account_id = var.service_account_id
+# }
+# ######
+# module "cloudbuild" {
+#   source = "./modules/cloud_build"
 #   project_id = var.project_id
 #   region = var.region
 #   project_number = var.project_number
+#   installation_id = var.installation_id
+#   cloud_build_github_pat = var.cloud_build_github_pat
+#   secret_id = var.secret_id
+#   remote_uri = var.remote_uri
 # }
+# ######
+# module "artifact_registry" {
+#   source = "./modules/artifact_registry"
+#   project_id = var.project_id
+#   region = var.region
+# }
+# ######
+# module "vertex_ai_workbench" {
+#   source = "./modules/vetex_ai_workbench"
+#   project_id = var.project_id
+#   service_account_email = var.service_account_id
+# }
+# ######
+# module "big-query" {
+#   source = "./modules/big_query"
+#   project_id = var.project_id
+#   region = var.region
+  
+# }
+# ######
+# module "cloud_function" {
+#   source = "./modules/cloud_funtions/metabringdatafunc"
+#   project_id = var.project_id
+#   region = var.region
+#   service_account_email = var.service_account_id
+#   table_id = "${var.project_id}.${module.big-query.meta_dataset_id}.${module.big-query.meta_table_id}"
+#   meta_api_secret_id = var.meta_api_secret_id
+#   meta_api_token = var.meta_api_token
+#   project_number = var.project_number
+# }
+# ##########
+# # module "datafrom_repository" {
+# #   source = "./modules/dataform"
+# #   project_id = var.project_id
+# #   region = var.region
+# #   project_number = var.project_number
+# # }
